2b8ab4f73c4a492ef676f1b57dea5791400433d9
Daniel Hurtado <81983296+DHurtado714-itesm@users.noreply.github.com>
2024-11-10 15:02:05 +0100

    refactor: Remove unused map_exchange_api_response function and ExchangeRate class from handler.py

--- handler.py
+++ handler.py
@@ -4,21 +4,13 @@ import os
 import requests
 from enum import Enum
 
+from exchange_rate_provider import ExchangeRate
+
 
 class NotionProperties(Enum):
     ID = "id"
     NUMBER = "number"
     SELECT = "select"
-
-
-def map_exchange_api_response(
-    rates: dict, base_currency: str, selected_currencies: list
-) -> dict:
-    return {
-        f"{currency}/{base_currency}": rates[currency]
-        for currency in selected_currencies
-        if currency in rates
-    }
 
 
 def map_properties_from_notion_response(response: dict, properties: dict) -> list:
@@ -52,25 +44,6 @@ def map_properties_from_notion_response(response: dict, properties: dict) -> lis
             results.append(entry)
 
     return results
-
-
-class ExchangeRate:
-    def __init__(self) -> None:
-        self.base_currency = "USD"
-        self.selected_currencies = ["COP", "EUR", "MXN"]
-        self.base_url = f"https://open.er-api.com/v6/latest/{self.base_currency}"
-
-    def fetch_data(self):
-        response = requests.get(self.base_url)
-
-        if response.status_code == 200:
-            data = response.json()
-
-            return map_exchange_api_response(
-                data["rates"], self.base_currency, self.selected_currencies
-            )
-        else:
-            raise Exception("Fialed to fetch rates")
 
 
 class NotionManager:
