f364a76614d2fd09ede487f10d1233f3e0d87e50
Daniel Hurtado <81983296+DHurtado714-itesm@users.noreply.github.com>
2024-11-10 15:01:56 +0100

    fix: Update import statement for ExchangeRate in create_exchange_rate_entry_handler.py

--- create_exchange_rate_entry_handler.py
+++ create_exchange_rate_entry_handler.py
@@ -1,8 +1,8 @@
 import os
 
 from exchange_rate_entity import ExchangeRateEntity
+from exchange_rate_provider import ExchangeRate
 from exchange_rate_repository import ExchangeRateRepository
-from handler import ExchangeRate
 
 
 def create_exchange_rate_entry_handler(event, context):
