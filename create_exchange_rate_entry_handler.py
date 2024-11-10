ed414309763beb5ff9160bfc7d3abcc1555e94ea
Daniel Hurtado <81983296+DHurtado714-itesm@users.noreply.github.com>
2024-11-10 14:46:38 +0100

    feat: Implement create_exchange_rate_entry_handler for processing exchange rate entries

---
+++ create_exchange_rate_entry_handler.py
@@ -0,0 +1,29 @@
+import os
+
+from exchange_rate_entity import ExchangeRateEntity
+from exchange_rate_repository import ExchangeRateRepository
+from handler import ExchangeRate
+
+
+def create_exchange_rate_entry_handler(event, context):
+    table_name = os.getenv("EXCHANGE_RATE_TABLE_NAME", "ExchangeRates")
+
+    repository = ExchangeRateRepository(table_name=table_name)
+    exchange_rate = ExchangeRate()
+
+    try:
+        rates = exchange_rate.fetch_data()
+
+        exchange_rate_entity = ExchangeRateEntity(rates=rates)
+
+        result = repository.post_exchange_rate(exchange_rate_entity)
+
+        if result:
+            return {
+                "statusCode": 200,
+                "body": "Exchange rate entry created successfully.",
+            }
+        else:
+            return {"statusCode": 500, "body": "Failed to create exchange rate entry."}
+    except Exception as e:
+        return {"statusCode": 500, "body": f"An error occurred: {str(e)}"}
